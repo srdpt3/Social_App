ba8dc737c233d98b5110b2def81829317acf8285
Dustin yang <dustinyang@Dustinui-MBP.fios-router.home>
2019-08-25 23:18:53 -0400

    update

--- config/env/production.js
+++ config/env/production.js
@@ -371,7 +371,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://dustin-social-app.herokuapp.com/',
     internalEmailAddress: 'support@example.com',
 
     // mailgunDomain: 'mg.example.com',
